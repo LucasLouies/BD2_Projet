a8e279a8726ab2305d071a2bb8878c38b0fdcc1c
Moh111200 <129392937+Moh111200@users.noreply.github.com>
2023-12-06 10:34:55 +0100

    Update data_test.sql

--- data_test.sql
+++ data_test.sql
@@ -22,21 +22,22 @@ SELECT projet.valider_offre_stage('VIN1');
 SELECT projet.valider_offre_stage('VIN4');
 SELECT projet.valider_offre_stage('VIN5');
 
--- Ajout de mots-clés pour les offres de stage
-SELECT projet.ajouter_mot_cle('VIN3', 1);
-SELECT projet.ajouter_mot_cle('VIN5', 1);
 
 -- Assurez-vous que les étudiants et les offres de stage ont le même semestre
 -- Exemple : Les étudiants posent des candidatures pour des offres de stage avec le même semestre
 SELECT projet.poser_candidature('j.d@vinci.be','JSPENCORE', 'VIN4');
 SELECT projet.poser_candidature('m.d@vinci.be','JSPENCORE', 'VIN5');
 
-
 -- ajouter entreprise
 SELECT projet.encoder_entreprise('ULB','Universite LB','mdpE1','ulb@ulb.be','Bruxelles');
 
 -- encoder stage
 SELECT projet.encoder_offre_stage('stage javascript', 'ULB', 'Q2');
+
+
+-- Ajout de mots-clés pour les offres de stage
+SELECT projet.ajouter_mot_cle('VIN3', 'Java');
+SELECT projet.ajouter_mot_cle('VIN5', 'Java');
 
 -- valider stage
 SELECT projet.valider_offre_stage('ULB1');
